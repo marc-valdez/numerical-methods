052ed304b92d26be21b53ef6fcd9feb39ce5c424
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2026-02-17 11:51:28 +0800

    Create VALDEZ_newton_raphson_method.py

---
+++ Machine Problem 1/VALDEZ_newton_raphson_method.py
@@ -0,0 +1,23 @@
+# Define the function of the problem
+def f(x):
+    return (pow(x, 4)) - (10 * (pow(x, 2))) + 1
+
+# Define the first derivative of f(x)
+def f1(x):
+    return (4 * pow(x, 3)) - (20 * x)
+
+x = 4
+es = 0.001
+
+for i in range(1, 500):
+    fx = f(x)
+    f1x = f1(x)
+    dx = -(fx/f1x)
+
+    print(f"Iteration #{i}: x = {x}")
+    x = x + dx
+
+    tol = abs(dx / x) * 100
+    if tol < es:
+        print(f"\nThe root is {x}")
+        break
