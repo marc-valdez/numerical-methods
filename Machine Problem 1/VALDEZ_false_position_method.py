cf036ce1c74b630036503da716aa9b21565746ad
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2026-02-17 11:51:15 +0800

    Create VALDEZ_false_position_method.py

---
+++ Machine Problem 1/VALDEZ_false_position_method.py
@@ -0,0 +1,24 @@
+# Define the function of the problem
+def f(x):
+    return (pow(x, 4)) - (10 * (pow(x, 2))) + 1
+
+xl = 1
+xr = 4
+es = 0.001
+
+for i in range(1, 500):
+    fxl = f(xl)
+    fxr = f(xr)
+
+    x0 = xl
+    x1 = ((xl * fxr) - (xr * fxl)) / (fxr - fxl)
+
+    if f(x1) * fxr < 0:
+        xl = x1
+    else:
+        xr = x1
+
+    tol = (abs(xl - x0) / xl) * 100
+    if tol < es:
+        print(f"[Iteration #{i}] x = {xl}")
+        break
