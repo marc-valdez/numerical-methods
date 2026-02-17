7d77c7cee706b129ec97b287735cf05440e17181
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2026-02-17 11:51:07 +0800

    Create VALDEZ_bisection_method.py

---
+++ Machine Problem 1/VALDEZ_bisection_method.py
@@ -0,0 +1,25 @@
+# Define the function of the problem
+def f(x):
+    return (pow(x, 4)) - (10 * (pow(x, 2))) + 1
+
+# Sometimes, initial values of xl and xr don't converge to anything.
+xl = -1
+xr = 4
+es = 0.001
+
+for i in range(1, 500):
+    xm = (xl + xr) / 2
+    fxm = f(xm)
+    fxr = f(xr)
+
+    # Why fxr and not fxl?
+    if (fxm * fxr) > 0:
+        xr = xm
+    elif (fxm * fxr) < 0:
+        xl = xm
+
+    xn = (xl + xr) / 2
+    tol = (abs(xn - xm) / xn) * 100
+    if tol < es:
+        print(f"[Iteration #{i}] x = {xn}")
+        break
