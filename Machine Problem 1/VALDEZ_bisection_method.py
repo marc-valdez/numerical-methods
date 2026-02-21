2280afcd8cff9bc74c3c0bbd174683b5b92eb6d3
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2026-02-21 10:47:43 +0800

    Update VALDEZ_bisection_method.py

--- Machine Problem 1/VALDEZ_bisection_method.py
+++ Machine Problem 1/VALDEZ_bisection_method.py
@@ -6,24 +6,29 @@ https://en.wikipedia.org/wiki/Bisection_method
 def f(x):
     return (pow(x, 4)) - (10 * (pow(x, 2))) + 1
 
-# Sometimes, initial values of xl and xr don't converge to anything.
-xl = -1
-xr = 4
-es = 0.001
+def bisect(a, b, f, es):
+    for i in range(1, 1000):
+        c = (a + b) / 2
+        yc = f(c)
+        yb = f(b)
 
-for i in range(1, 500):
-    xm = (xl + xr) / 2
-    fxm = f(xm)
-    fxr = f(xr)
+        print(f"[Iteration #{i}] x = {c}, a = {a}, b = {b}")
 
-    # Why fxr and not fxl?
-    if (fxm * fxr) > 0:
-        xr = xm
-    elif (fxm * fxr) < 0:
-        xl = xm
+        if (yc * yb) > 0:
+            b = c
+        elif (yc * yb) < 0:
+            a = c
 
-    xn = (xl + xr) / 2
-    tol = (abs(xn - xm) / xn) * 100
-    if tol < es:
-        print(f"[Iteration #{i}] x = {xn}")
-        break
+        c1 = (a + b) / 2
+        tol = (abs(c1 - c) / c1) * 100
+        if tol < es:
+            return c1
+
+
+if __name__ == "__main__":
+    a = 3
+    b = 4
+    es = 0.001
+
+    root = bisect(a, b, f, es)
+    print(f"\nThe root is {root}")
