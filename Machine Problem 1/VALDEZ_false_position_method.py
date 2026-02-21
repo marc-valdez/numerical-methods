d2cc1bc72d3de1e4b9554ec889978d84c4c3355f
Marc Valdez <127009880+marc-valdez@users.noreply.github.com>
2026-02-21 10:59:22 +0800

    Update VALDEZ_false_position_method.py

--- Machine Problem 1/VALDEZ_false_position_method.py
+++ Machine Problem 1/VALDEZ_false_position_method.py
@@ -6,23 +6,30 @@ https://en.wikipedia.org/wiki/Regula_falsi
 def f(x):
     return (pow(x, 4)) - (10 * (pow(x, 2))) + 1
 
-xl = 1
-xr = 4
-es = 0.001
+def regula_falsi(a, b, f, es):
+    for i in range(1, 1000):
+        ya = f(a)
+        yb = f(b)
 
-for i in range(1, 500):
-    fxl = f(xl)
-    fxr = f(xr)
+        a0 = a
+        c = ((a * yb) - (b * ya)) / (yb - ya)
 
-    x0 = xl
-    x1 = ((xl * fxr) - (xr * fxl)) / (fxr - fxl)
+        print(f"[Iteration #{i}] x = {a}, a = {a}, b = {b}")
 
-    if f(x1) * fxr < 0:
-        xl = x1
-    else:
-        xr = x1
+        if f(c) * yb < 0:
+            a = c
+        else:
+            b = c
 
-    tol = (abs(xl - x0) / xl) * 100
-    if tol < es:
-        print(f"[Iteration #{i}] x = {xl}")
-        break
+        tol = (abs(c - a0) / c) * 100
+        if tol < es:
+            return c
+
+
+if __name__ == "__main__":
+    a = 3
+    b = 4
+    es = 0.001
+
+    root = regula_falsi(a, b, f, es)
+    print(f"\nThe root is {root}")
